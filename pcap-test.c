8e521c4b85764bbc30503d13943c6b671c69b720
jammy0903 <fuso3367@kakao.com>
2024-03-17 05:47:34 -0400

    pcap-test 0317

--- pcap-test.c
+++ pcap-test.c
@@ -7,115 +7,12 @@
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 
+struct ether_header *eth_hdr;
+struct tcphdr *tcp_hdr;
+struct ip *ip_hdr;
 
-
-#ifndef ETHERTYPE_IP
-#define ETHERTYPE_IP            0x0800  /* IP protocol */
-#endif
-
-
-struct libnet_ipv4_hdr
-{
-#if (LIBNET_LIL_ENDIAN)
-    u_int8_t ip_hl:4,      /* header length */
-        ip_v:4;         /* version */
-#endif
-#if (LIBNET_BIG_ENDIAN)
-    u_int8_t ip_v:4,       /* version */
-        ip_hl:4;        /* header length */
-#endif
-    u_int8_t ip_tos;       /* type of service */
-#ifndef IPTOS_LOWDELAY
-#define IPTOS_LOWDELAY      0x10
-#endif
-#ifndef IPTOS_THROUGHPUT
-#define IPTOS_THROUGHPUT    0x08
-#endif
-#ifndef IPTOS_RELIABILITY
-#define IPTOS_RELIABILITY   0x04
-#endif
-#ifndef IPTOS_LOWCOST
-#define IPTOS_LOWCOST       0x02
-#endif
-    u_int16_t ip_len;         /* total length */
-    u_int16_t ip_id;          /* identification */
-    u_int16_t ip_off;
-#ifndef IP_RF
-#define IP_RF 0x8000        /* reserved fragment flag */
-#endif
-#ifndef IP_DF
-#define IP_DF 0x4000        /* dont fragment flag */
-#endif
-#ifndef IP_MF
-#define IP_MF 0x2000        /* more fragments flag */
-#endif
-#ifndef IP_OFFMASK
-#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
-#endif
-    u_int8_t ip_ttl;          /* time to live */
-    u_int8_t ip_p;            /* protocol */
-    u_int16_t ip_sum;         /* checksum */
-    struct in_addr ip_src, ip_dst; /* source and dest address */
-};
-
-          
-struct libnet_ethernet_hdr
-{
-    u_int8_t  ether_dhost[ETHER_ADDR_LEN];/* destination ethernet address */
-    u_int8_t  ether_shost[ETHER_ADDR_LEN];/* source ethernet address */
-    u_int16_t ether_type;                 /* protocol */
-};
-struct libnet_tcp_hdr
-{
-    u_int16_t th_sport;       /* source port */
-    u_int16_t th_dport;       /* destination port */
-    u_int32_t th_seq;          /* sequence number */
-    u_int32_t th_ack;          /* acknowledgement number */
-#if (LIBNET_LIL_ENDIAN)
-    u_int8_t th_x2:4,         /* (unused) */
-           th_off:4;        /* data offset */
-#endif
-#if (LIBNET_BIG_ENDIAN)
-    u_int8_t th_off:4,        /* data offset */
-           th_x2:4;         /* (unused) */
-#endif
-    u_int8_t  th_flags;       /* control flags */
-#ifndef TH_FIN
-#define TH_FIN    0x01      /* finished send data */
-#endif
-#ifndef TH_SYN
-#define TH_SYN    0x02      /* synchronize sequence numbers */
-#endif
-#ifndef TH_RST
-#define TH_RST    0x04      /* reset the connection */
-#endif
-#ifndef TH_PUSH
-#define TH_PUSH   0x08      /* push data to the app layer */
-#endif
-#ifndef TH_ACK
-#define TH_ACK    0x10      /* acknowledge */
-#endif
-#ifndef TH_URG
-#define TH_URG    0x20      /* urgent! */
-#endif
-#ifndef TH_ECE
-#define TH_ECE    0x40
-#endif
-#ifndef TH_CWR   
-#define TH_CWR    0x80
-#endif
-    u_int16_t th_win;    
-};
-
-
-struct ether_header *eth_h;
-struct tcphdr *tcp_h;
-struct ip *ip_h;
-
-
-
-
-
+//tcp header start as 08 00
+#define TCP
 
 
 
@@ -157,13 +54,37 @@ int main(int argc, char* argv[]) {
 	while (true) {
 		struct pcap_pkthdr* header;
 		const u_char* packet;
-		int res = pcap_next_ex(pcap, &header, &packet);
+        int res = pcap_next_ex(pcap, &header, &packet); // pck_hdr's ptr's ptr=>header's wichi  && packet pointer
+        //'header' include metadata.
+        //'packet' include packet data
 		if (res == 0) continue;
 		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
 			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
 			break;
 		}
 		printf("%u bytes captured\n", header->caplen);
+
+        /*  이더넷 패킷헤더 포인터 가리키기
+                    이더넷 길이는 정해져있으므로 구할 필요 x
+                    이더넷 mac 주소 구하기 (src, dst)
+                    IP헤더 위치 구하기 packet + sizeof(struct ether_header)
+                    IP헤더 길이 구하기 ip_hl 를 구하고 바이트로 변환해야함.
+                    IP src,dst 구하기
+                    TCP헤더 위치 구하기(ip길이로)
+                    tcp 인지아닌지 판별
+                    tcp이면 -> src port / dst port 출력
+                    else-> continue (?문법 맞는지 확인헤야함)
+                    payload  hexadecimal value for문돌려서 10까지만 출력
+
+                */
+
+           eth_hdr= (struct ether_header*)packet; //pkt chg into header's ptr
+           ip_hdr = (struct ip*)(packet + sizeof(struct ether_header));
+           tcp_hdr= (struct tcphdr*)(packet +sizeof(struct ip));
+
+           if (ip_hdr->ip_p == IPPROTO_TCP) {
+               printf("ethernet header SRC MAC address : %s \n", ether_ntoa((const struct ether_addr*)eth_hdr->ether_shost));
+           }else{continue;}
 	}
 
 	pcap_close(pcap);
